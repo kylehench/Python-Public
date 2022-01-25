2df0ba74a796ed11ff77b89f2a40f5cc92993475
Kyle Hench <kylehench@gmail.com>
2022-01-25 15:07:30 -0800

    added palindrome_checker.py and modified math_dojo.py

---
+++ algo-practice/palindrome_checker.py
@@ -0,0 +1,11 @@
+def pal_check_concise(word):
+    # concise palindrome checker
+    print('Word: ', word)
+    # slice a string in reverse: string[end:start:-1]
+    # start is non-inclusive in reverse! Ok for odd length strings because middle char does not need to be tested
+    # need to reduce by one for reverse test if length is even: -(len(word)+1)%2
+    return word[:int(len(word)/2)-(len(word)+1)%2:-1].lower() == word[:int(len(word)/2)].lower()
+
+test_words = ['bob','Bob','palindromeemordnilap','not','nada','y','no']
+for word in test_words:
+    print(pal_check_concise(word),'\n')
--- fundamentals/extras/math_dojo.py
+++ fundamentals/extras/math_dojo.py
@@ -17,3 +17,5 @@ md = MathDojo()
 x = md.add(2).add(2,5,1).subtract(3,2).result
 print(x)	# should print 5
 # run each of the methods a few more times and check the result!
+print(md.add(1).add(2,2).add(3,3,3).result) # should print 19
+print(md.subtract(1).subtract(2,2).subtract(3,3,3).result) # should print 5
