13c3d2e4de7b9806c4e6f0d46753d059ffaa146a
Kyle Hench <kylehench@gmail.com>
2022-01-25 19:10:38 -0800

    updated palidrome_checker.py

--- algo-practice/palindrome_checker.py
+++ algo-practice/palindrome_checker.py
@@ -1,11 +1,19 @@
-def pal_check_concise(word):
-    # concise palindrome checker
+def pal_check1(word):
+    # palindrome checker
     print('Word: ', word)
     # slice a string in reverse: string[end:start:-1]
     # start is non-inclusive in reverse! Ok for odd length strings because middle char does not need to be tested
     # need to reduce by one for reverse test if length is even: -(len(word)+1)%2
     return word[:int(len(word)/2)-(len(word)+1)%2:-1].lower() == word[:int(len(word)/2)].lower()
 
+def pal_check2(word):
+    # palindrome checker
+    print('Word: ', word)
+    return word.lower() == word[::-1].lower()
+
+
 test_words = ['bob','Bob','palindromeemordnilap','not','nada','y','no']
+# test_words = ['bob']
 for word in test_words:
-    print(pal_check_concise(word),'\n')
+    print(pal_check1(word),'\n')
+    print(pal_check2(word),'\n')
