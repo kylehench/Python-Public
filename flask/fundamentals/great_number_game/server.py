40d5cd256868e45623611280cdee0706ab188dbe
Kyle Hench <kylehench@gmail.com>
2022-02-01 12:32:53 -0800

    added number guessing game

---
+++ flask/fundamentals/great_number_game/server.py
@@ -0,0 +1,24 @@
+from distutils.log import debug
+from flask import Flask, render_template, request, redirect, session
+import random
+app = Flask(__name__)
+app.secret_key = 'someThIngHRdTooGUeSS593849#*%&^@^$%'
+
+@app.route('/')
+def index():
+    if 'number' not in session:
+        session['number'] = random.randint(1,100)
+    return render_template('index.html')
+
+@app.route('/guess', methods=['post'])
+def guess():
+    session['guess'] = int(request.form['guess'])
+    return redirect('/')
+
+@app.route('/reset')
+def reset():
+    session.clear()
+    return redirect('/')
+
+if __name__=='__main__':
+    app.run(debug=True)
