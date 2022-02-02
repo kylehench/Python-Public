7f65746781798353d04b1e245f7dad4cdc72ab98
Kyle Hench <kylehench@gmail.com>
2022-02-02 15:11:13 -0800

    updated dojo_survey

--- flask/fundamentals/dojo_survery/server.py
+++ flask/fundamentals/dojo_survery/server.py
@@ -3,21 +3,30 @@ from flask import Flask, render_template, request, redirect, session
 app = Flask(__name__)
 app.secret_key = 'jlujknohkjmjlnkdfwcsrsdsacsfdfxd54525488793948539'
 
-loc_dict = {}
+loc_list = ['Online', 'Bellevue', 'Burbank', 'Chicago', 'San Jose', 'Seattle']
+
+lang_list = ['HTML', 'CSS', 'JavaScript', 'Python', 'Java']
+
+form_list = ['Name', 'Location', 'Language','Pacing', 'Comments']
+
+pacing_list = ['Just right', 'A little fast', 'A little slow']
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', loc_list=loc_list, lang_list=lang_list, pacing_list=pacing_list)
 
-@app.route('/process-survey', methods=['POST'])
+@app.route('/process-survey', methods=['post'])
 def process():
-    session['name'] = request.form['name']
-    print(session)
+    for value in form_list:
+        if value in request.form:
+            session[value] = request.form[value]
+        else:
+            session[value] = 'null'
     return redirect('/result')
 
 @app.route('/result')
 def result():
-    return render_template('result.html')
+    return render_template('result.html', form_list=form_list)
     
 if __name__=='__main__':
     app.run(debug=True)
