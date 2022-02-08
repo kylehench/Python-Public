8dadb86b882d28fe67f2d3c05b6097083ba964b8
Kyle Hench <kylehench@gmail.com>
2022-02-08 13:40:39 -0800

    users_crud completed

--- flask_mysql/crud/users_crud/server.py
+++ flask_mysql/crud/users_crud/server.py
@@ -16,6 +16,32 @@ def users():
 def new_user():
   return render_template('/new_user.html')
 
+@app.route('/users/<id>')
+def show_user(id):
+  user = User.get_id(id)[0]
+  return render_template('/show_user.html', user=user)
+
+@app.route('/users/<id>/edit')
+def edit_user(id):
+  user = User.get_id(id)[0]
+  return render_template('/update_user.html', id=id, user=user)
+
+@app.route('/process/users/<id>/edit', methods=['POST'])
+def process_user_edit(id):
+  data = {
+    "first_name" : request.form['first_name'],
+    "last_name" : request.form['last_name'],
+    "email" : request.form['email'],
+    "id" : id
+  }
+  User.update_id(data)
+  return redirect(f'/users/{id}')
+
+@app.route('/users/<id>/delete')
+def delete_user(id):
+  User.delete_id(id)
+  return redirect('/users')
+
 @app.route('/process', methods=['POST'])
 def process():
   data = {
@@ -23,8 +49,8 @@ def process():
     "last_name" : request.form['last_name'],
     "email" : request.form['email']
   }
-  User.save(data)
-  return redirect('/users')
+  id = User.save(data)
+  return redirect(f'/users/{id}')
 
 if __name__=='__main__':
   app.run(debug=True)
--- flask_mysql/crud/users_crud/user.py
+++ flask_mysql/crud/users_crud/user.py
@@ -19,9 +19,33 @@ class User:
       users.append(cls(user))
     return users
 
+  # class method to query one user from our database
+  @classmethod
+  def get_id(cls, id):
+    data = {'id' : id}
+    query = "SELECT * from users WHERE id= %(id)s ;"
+    results = connectToMySQL('users_schema').query_db(query, data)
+    users = []
+    for user in results:
+      users.append(cls(user))
+    return users
+
   # class method to save our user to the database
   @classmethod
-  def save(cls, data ):
+  def save(cls, data):
       query = "INSERT INTO users ( first_name , last_name , email , created_at, updated_at ) VALUES ( %(first_name)s , %(last_name)s , %(email)s , NOW() , NOW() );"
       # data is a dictionary that will be passed into the save method from server.py
       return connectToMySQL('users_schema').query_db( query, data )
+
+  # class method to update a user in the database
+  @classmethod
+  def update_id(cls, data):
+      query = "UPDATE users SET first_name = %(first_name)s, last_name = %(last_name)s, email = %(email)s, updated_at = NOW() WHERE id=%(id)s;"
+      connectToMySQL('users_schema').query_db(query, data)
+
+  # class method to delete a user in the database
+  @classmethod
+  def delete_id(cls, id):
+      data = {'id' : id}
+      query = "DELETE FROM users WHERE id= %(id)s ;"
+      connectToMySQL('users_schema').query_db(query, data)
