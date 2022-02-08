d03fde3d7cdac23058b85c928e6c86a8c9981f6a
Kyle Hench <kylehench@gmail.com>
2022-02-07 16:16:46 -0800

    updated first_flask_mysql

--- flask_mysql/db_connection/first_flask_mysql/friend.py
+++ flask_mysql/db_connection/first_flask_mysql/friend.py
@@ -9,6 +9,7 @@ class Friend:
         self.occupation = data['occupation']
         self.created_at = data['created_at']
         self.updated_at = data['updated_at']
+        
     # Now we use class methods to query our database
     @classmethod
     def get_all(cls):
@@ -21,3 +22,10 @@ class Friend:
         for friend in results:
             friends.append( cls(friend) )
         return friends
+
+    # class method to save our friend to the database
+    @classmethod
+    def save(cls, data ):
+        query = "INSERT INTO friends ( first_name , last_name , occupation , created_at, updated_at ) VALUES ( %(fname)s , %(lname)s , %(occ)s , NOW() , NOW() );"
+        # data is a dictionary that will be passed into the save method from server.py
+        return connectToMySQL('first_flask').query_db( query, data )
