f9e5629849eff38a5d1b864cdd0ba3b25782fa9e
Kyle Hench <kylehench@gmail.com>
2022-02-09 13:43:02 -0800

    updated counting_sort.py algo

--- algo-practice/012_counting_sort.py
+++ algo-practice/012_counting_sort.py
@@ -7,6 +7,7 @@
 
 # Alternative Sorting
 # Another sorting method, the counting sort, does not require comparison. Instead, you create an integer array whose index range covers the entire range of values in your array to sort. Each time a value occurs in the original array, you increment the counter at that index. At the end, run through your counting array, printing the value of each non-zero valued index that number of times.
+# Returns: int[100]: a frequency array
 
 def countingSort(arr):
   result = [0 for i in range(100)]
