ee2eda7539a73510f26d316fc9ba7951c853ce4e
Kyle Hench <kylehench@gmail.com>
2022-03-01 16:37:17 -0800

    fixed getUsers()

--- ajax/ajax_flask/form_data/flask_app/static/js/site.js
+++ ajax/ajax_flask/form_data/flask_app/static/js/site.js
@@ -20,7 +20,7 @@ function getUsers(){
 }
 
 var myForm = document.getElementById('myForm');
-myForm.onsubmit = function(e){
+myForm.onsubmit = function(e) {
     // "e" is the js event happening when we submit the form.
     // e.preventDefault() is a method that stops the default nature of javascript.
     e.preventDefault();
@@ -29,6 +29,7 @@ myForm.onsubmit = function(e){
     // this how we set up a post request and send the form data.
     fetch("http://127.0.0.1:5000/create/user", { method :'POST', body : form})
         .then( response => response.json() )
-        .then( data => console.log(data) )
-    getUsers();
-}
+        .then( data => {console.log(data);
+            getUsers();
+        })
+    }
