2349d92073106ec51ab0c978a83036cbba2b4cd5
Kyle Hench <kylehench@gmail.com>
2022-05-09 11:08:25 -0700

    algo

--- algo-practice/042_fibonacci_modified.py
+++ algo-practice/042_fibonacci_modified.py
@@ -1,7 +1,18 @@
-def fibonacciModified(t1, t2, n):
+import timeit
+import math
+
+def fibonacciModified1(t1, t2, n):
   arr = [t1, t2]
   for i in range(n-2):
     arr.append(arr[i]+arr[i+1]**2)
   return arr[-1]
 
-print(fibonacciModified(0,1,6))
+def fibonacciModified2(t1, t2, n):
+  t3 = t1 + t2**2
+  for _ in range(n-2):
+    t1, t2, t3 = t2, t3, t1 + t2**2
+  return t3
+
+n = 28
+print(timeit.timeit(lambda: fibonacciModified1(0,1,n), number=1))
+print(timeit.timeit(lambda: fibonacciModified2(0,1,n), number=1))
