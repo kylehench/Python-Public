3600b90755d698d2b010fa14370b808a948dcd73
Kyle Hench <kylehench@gmail.com>
2022-05-25 11:08:21 -0700

    algo

---
+++ algo-practice/testing_snippets_and_notes.py
@@ -0,0 +1,21 @@
+# hard-coded input
+test_cases = ('',)
+test_results = (1,)
+
+# file input
+with open(r"file_path") as f:
+  test_cases = f.readlines()
+with open(r"file_path") as f:
+  test_results = f.readlines()
+test_cases = [s.strip() for s in test_cases[1:]]
+test_results = [s.strip() for s in test_results]
+
+# test
+all_pass = True
+for i, (case, result) in enumerate(zip(test_cases, test_results)):
+  my_result = function(case)
+  if  my_result != result:
+    all_pass = False
+    print(f'FAIL. My result: {my_result}. Expected result: {result}. Index: {i}')
+if all_pass == True:
+  print('ALL PASS')
