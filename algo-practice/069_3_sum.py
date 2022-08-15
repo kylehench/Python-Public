b6666042129bfe17aeab16b9c064821787a1cfb4
Kyle Hench <kylehench@gmail.com>
2022-08-15 16:43:06 -0700

    algo

--- algo-practice/069_3_sum.py
+++ algo-practice/069_3_sum.py
@@ -2,24 +2,18 @@ class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         nums.sort()
         triplets = []
-        for i, target in list(enumerate(nums))[:-1]:
-            # if target == nums[i+1]:
-                # continue
+        for i in range(0, len(nums)-1):
             j = i+1
             k = len(nums)-1
             while j != k:
                 partial_sum = nums[j] + nums[k]
-                if partial_sum < -target:
+                if partial_sum < -nums[i]:
                     j += 1
-                elif partial_sum > -target:
+                elif partial_sum > -nums[i]:
                     k -= 1
-                    # while nums[k]==nums[k+1]:
-                        # k -= 1
                 else:
                     triplets.append([nums[i], nums[j], nums[k]])
                     j += 1
-                    # while nums[j]==nums[j-1]:
-                        # j += 1
         triplets.sort()
         if len(triplets) > 0:
             res = [triplets[0]]
