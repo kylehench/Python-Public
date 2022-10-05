5cb2e32eff225840267fb31ce72e91032b1b0c6f
Kyle Hench <kylehench@gmail.com>
2022-10-05 12:11:46 -0700

    algo

--- algo-practice/096_subsets.py
+++ algo-practice/096_subsets.py
@@ -2,7 +2,7 @@
 
 # The solution set must not contain duplicate subsets. Return the solution in any order.
 
-class Solution:
+class Solution1:
   def subsets(self, nums: List[int]) -> List[List[int]]:
     def extend(arr, elements):
       if len(elements)==0:
@@ -11,3 +11,10 @@ class Solution:
       arr.extend([item + [next_el] for item in arr])
       return extend(arr, elements)
     return extend([[]], nums)
+
+class Solution2:
+  def subsets(self, nums: List[int]) -> List[List[int]]:
+    res = [[]]
+    for num in nums:
+      res.extend([item + [num] for item in res])
+    return res
