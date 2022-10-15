79b9166dd34a0a9ccfc80f84b0ee5d6c3bbe1e8f
Kyle Hench <kylehench@gmail.com>
2022-10-15 14:18:10 -0700

    algo

--- algo-practice/099_best_time_to_buy_and_sell_stock.py
+++ algo-practice/099_best_time_to_buy_and_sell_stock.py
@@ -3,5 +3,20 @@
 # Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
 
 class Solution:
-    def maxProfit(self, prices: List[int]) -> int:
-        
+  def maxProfit(self, prices: List[int]) -> int:
+    maximum = prices[-1]
+    max_arr = [maximum]
+    for i in range(len(prices)-2, -1, -1):
+      if prices[i] > maximum:
+        maximum = prices[i]
+      max_arr.append(maximum)  
+    max_arr.reverse()
+    
+    max_profit = 0
+    minimum = prices[0]
+    for i in range(len(prices)):
+      if prices[i] < minimum:
+        minimum = prices[i]
+      if max_arr[i]-minimum > max_profit:
+        max_profit = max_arr[i]-minimum
+    return max_profit
