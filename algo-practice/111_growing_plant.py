d0228f7f1ba1289a39ffba410d384897f6685f02
Kyle Hench <kylehench@gmail.com>
2022-11-17 22:51:50 -0800

    algo

---
+++ algo-practice/111_growing_plant.py
@@ -0,0 +1,11 @@
+# Caring for a plant can be hard work, but since you tend to it regularly, you have a plant that grows consistently. Each day, its height increases by a fixed amount represented by the integer upSpeed. But due to lack of sunlight, the plant decreases in height every night, by an amount represented by downSpeed.
+# Since you grew the plant from a seed, it started at height 0 initially. Given an integer desiredHeight, your task is to find how many days it'll take for the plant to reach this height.
+# Example
+# For upSpeed = 100, downSpeed = 10, and desiredHeight = 910, the output should be solution(upSpeed, downSpeed, desiredHeight) = 10.
+
+import math
+
+def solution(upSpeed, downSpeed, desiredHeight):
+  if desiredHeight < downSpeed:
+    return 1
+  return math.ceil((desiredHeight - downSpeed)/(upSpeed-downSpeed))
